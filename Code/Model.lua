e9b280b633724a1bc9b3f7d0694b541e071e9a9a
pmalonis <pmalonis@uchicago.edu>
2016-01-20 21:27:53 -0600

    created model

--- Code/Model.lua
+++ Code/Model.lua
@@ -2,5 +2,22 @@ require 'nn'
 
 -- Define the two-layer perceptron and the loss function.
 -- Your code goes here 
+nInput = 4096
+nHidden = 1000
+nOutput = 20
+
+dropoutRatio = 0.5
+
 model = nn.Sequential()
+model:add(nn.Reshape(nInput))
+model:add(nn.Linear(nInput,nHidden))
+model:add(nn.ReLU(true))
+model:add(nn.Dropout(dropoutRatio))
+model:add(nn.Linear(nHidden, nOutput))
+
+model:add(nn.LogSoftMax())
+
+criterion = nn.ClassNLLCriterion()
+
+
 
